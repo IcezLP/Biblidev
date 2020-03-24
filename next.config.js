c0b42aac72a0e92f4adfc84fadb80abb3e79cd11
IcezLP <jo-brasier@outlook.com>
2020-03-24 12:12:57 +0100

    Installation de Sass et Svgr

--- next.config.js
+++ next.config.js
@@ -4,4 +4,15 @@ module.exports = {
   env: {
     SERVER_PORT: process.env.SERVER_PORT,
   },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      issuer: {
+        test: /\.(js|ts)x?$/,
+      },
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  },
 };
