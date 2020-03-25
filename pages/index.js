b77753192ed8e604617ce41b08aa423229ecae0b
IcezLP <jo-brasier@outlook.com>
2020-03-25 23:32:16 +0100

    Réinitialisation

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,3 @@
 import React from 'react';
-import Axios from 'axios';
 
-export default () => {
-  const request = async () => {
-    try {
-      const response = await Axios.post('/api/auth/login');
-      console.log(response);
-    } catch (error) {
-      console.log(error.response);
-    }
-  };
-  return (
-    <button type="button" onClick={request}>
-      Request
-    </button>
-  );
-};
+export default () => <div />;
