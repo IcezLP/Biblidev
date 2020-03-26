cd7fa82242619534099e38c3dd3382ded83fdc35
IcezLP <jo-brasier@outlook.com>
2020-03-26 10:58:17 +0100

    Function de récupération de données par id

---
+++ routes/users.js
@@ -0,0 +1,39 @@
+const router = require('express').Router();
+const User = require('../database/models/User');
+const authenticateJwt = require('../middlewares/authenticateJwt');
+
+/**
+ * Récupère les information d'un utilisateur via son id
+ *
+ * @async
+ * @route POST /api/users/:id
+ * @public
+ * @return {Object} Informations utilisateur
+ */
+router.get('/:id', async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id, { password: 0 });
+
+    if (!user) {
+      return res.status(400).json({
+        status: 'error',
+        data: {},
+        message: "Aucun utilisateur n'a été trouvé",
+      });
+    }
+
+    return res.status(200).json({
+      status: 'success',
+      data: user,
+      message: null,
+    });
+  } catch (error) {
+    return res.status(400).json({
+      status: 'error',
+      data: {},
+      message: 'Une erreur est survenue, veuillez réesayer',
+    });
+  }
+});
+
+module.exports = router;
