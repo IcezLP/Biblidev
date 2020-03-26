db5234c38277cf7a2ed2bd96d5f3733556fc0f76
IcezLP <jo-brasier@outlook.com>
2020-03-26 10:59:40 +0100

    Aucune modification

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,4 @@
 import React from 'react';
 import withAuth from '../middlewares/withAuth';
-import fetch from '../lib/fetch';
 
-export default withAuth(() => {
-  const test = async () => {
-    await fetch('post', '/api/users/test');
-  };
-
-  return <button onClick={test}>Test</button>;
-});
+export default withAuth(() => <div />);
