d034a880f17a42bf10c6c384235e7f6fb552a568
IcezLP <jo-brasier@outlook.com>
2020-04-03 11:40:53 +0200

    Ajout de ant design

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
+const withCss = require('@zeit/next-css');
 
-module.exports = {
+module.exports = withCss({
   env: {
     SERVER_PORT: process.env.SERVER_PORT,
     MONGO_URI: process.env.MONGO_URI,
@@ -8,7 +9,8 @@ module.exports = {
     SITE_URL: process.env.SITE_URL,
     TOKEN_SECRET: process.env.TOKEN_SECRET,
   },
-  webpack(config) {
+  webpack: (config, { isServer }) => {
+    // @svgr/webpack config
     config.module.rules.push({
       test: /\.svg$/,
       issuer: {
@@ -17,6 +19,28 @@ module.exports = {
       use: ['@svgr/webpack'],
     });
 
+    // Antd config
+    if (isServer) {
+      const antStyles = /antd\/.*?\/style\/css.*?/;
+      const origExternals = [...config.externals];
+      config.externals = [
+        (context, request, callback) => {
+          if (request.match(antStyles)) return callback();
+          if (typeof origExternals[0] === 'function') {
+            origExternals[0](context, request, callback);
+          } else {
+            callback();
+          }
+        },
+        ...(typeof origExternals[0] === 'function' ? [] : origExternals),
+      ];
+
+      config.module.rules.unshift({
+        test: antStyles,
+        use: 'null-loader',
+      });
+    }
+
     return config;
   },
-};
+});
