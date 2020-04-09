00934447a6c914a59c8e6da538b35afd4e96819c
IcezLP <jo-brasier@outlook.com>
2020-04-09 18:36:50 +0200

    Maintenance

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Result } from 'antd';
 import withAuth from '../middlewares/withAuth';
 
-export default withAuth(() => <div>Maintenance en cours</div>);
+export default withAuth(() => <Result title="Maintenance en cours" />);
