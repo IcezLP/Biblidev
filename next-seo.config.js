f9d6dd71944f49499e70a1daef810c3204608a9b
IcezLP <jo-brasier@outlook.com>
2020-04-15 10:24:07 +0200

    Ajout d'une image à OpenGraph

--- next-seo.config.js
+++ next-seo.config.js
@@ -8,5 +8,13 @@ export default {
     locale: 'fr_FR',
     url: process.env.SITE_URL,
     site_name: process.env.SITE_NAME,
+    images: [
+      {
+        url: `${process.env.SITE_URL}/images/logo_block.svg`,
+        width: 800,
+        height: 600,
+        alt: process.env.SITE_NAME,
+      },
+    ],
   },
 };
