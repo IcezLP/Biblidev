5be12a54309a893ae0748d357d9faf067508119a
IcezLP <jo-brasier@outlook.com>
2020-04-19 16:20:55 +0200

    Responsive du layout admin

--- components/layout/admin/Layout.js
+++ components/layout/admin/Layout.js
@@ -2,9 +2,11 @@ import React, { useState, useEffect } from 'react';
 import { Layout } from 'antd';
 import Sidebar from './Sidebar';
 import Header from './Header';
+import useWindowSize from '../../../hooks/useWindowSize';
 
 export default ({ children, title, subTitle }) => {
   const [collapsed, setCollapsed] = useState(false);
+  const { width } = useWindowSize();
 
   useEffect(() => {
     let storedCollapsedState = localStorage.getItem('collapsed');
@@ -20,10 +22,21 @@ export default ({ children, title, subTitle }) => {
     setCollapsed(!collapsed);
   };
 
+  const marginLeft = () => {
+    if (collapsed || (width && width < 1200)) {
+      return '80px';
+    }
+
+    return '200px';
+  };
+
   return (
     <Layout className="admin" hasSider>
       <Sidebar collapsed={collapsed} onCollapse={collapse} />
-      <Layout.Content className="admin__content">
+      <Layout.Content
+        className="admin__content"
+        style={{ marginLeft: marginLeft(), transition: width > 1200 && 'margin 0.2s' }}
+      >
         <Header title={title} subTitle={subTitle} />
         {children}
       </Layout.Content>
