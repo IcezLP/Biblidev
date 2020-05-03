262aababd848fd9e2a912108144765380e5d89c4
IcezLP <jo-brasier@outlook.com>
2020-05-03 12:34:31 +0200

    Ajout de commentaires et validation des props

--- components/pages/home/SearchBar.js
+++ components/pages/home/SearchBar.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import { Row, Col, Tag, Input, Select } from 'antd';
+import { Row, Col, Input, Select } from 'antd';
+import PropTypes from 'prop-types';
 
 const { memo } = React;
 const { Search } = Input;
 
+// Options de tri des ressources
 const sortOptions = [
   {
     label: 'Ordre alphabétique',
@@ -33,25 +35,32 @@ const sortOptions = [
   },
 ];
 
-const SearchBar = ({ selectedOption, onSearch, onSortChange }) => {
-  return (
-    <>
-      <Row gutter={[12, 12]}>
-        <Col xs={24} sm={14} md={16} lg={18} xxl={20}>
-          <Search placeholder="Rechercher..." onSearch={onSearch} />
-        </Col>
-        <Col xs={24} sm={10} md={8} lg={6} xxl={4}>
-          <Select
-            options={sortOptions}
-            value={selectedOption}
-            style={{ width: '100%' }}
-            placeholder="Trier par"
-            onChange={onSortChange}
-          />
-        </Col>
-      </Row>
-    </>
-  );
+const SearchBar = ({ selectedOption, onSearch, onSortChange }) => (
+  <Row gutter={[12, 12]}>
+    <Col xs={24} sm={14} md={16} lg={18} xxl={20}>
+      <Search placeholder="Rechercher..." onSearch={onSearch} />
+    </Col>
+    <Col xs={24} sm={10} md={8} lg={6} xxl={4}>
+      <Select
+        options={sortOptions}
+        value={selectedOption}
+        style={{ width: '100%' }}
+        placeholder="Trier par"
+        onChange={onSortChange}
+      />
+    </Col>
+  </Row>
+);
+
+// Validation des props
+SearchBar.propTypes = {
+  selectedOption: PropTypes.arrayOf({
+    label: PropTypes.string.isRequired,
+    value: PropTypes.string.isRequired,
+    disabled: PropTypes.bool,
+  }).isRequired,
+  onSearch: PropTypes.func.isRequired,
+  onSortChange: PropTypes.func.isRequired,
 };
 
 export default memo(SearchBar);
