adaf48dbdbdade7c1442049adc79180a6835677c
IcezLP <jo-brasier@outlook.com>
2020-05-07 21:04:40 +0200

    Rangement du code

---
+++ components/layout/global/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Layout } from 'antd';
+
+const { memo } = React;
+
+const Footer = () => {
+  // Récupère l'année en cours
+  const actualYear = new Date().getFullYear();
+
+  return (
+    <Layout.Footer style={{ textAlign: 'center' }}>
+      {/* eslint-disable-next-line */}
+      Biblidev 2019 - {actualYear}
+    </Layout.Footer>
+  );
+};
+
+export default memo(Footer);
