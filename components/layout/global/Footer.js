b23f7e9b941046bce6a72bc658ea06d40fd7ff9b
IcezLP <jo-brasier@outlook.com>
2020-05-08 15:06:17 +0200

    Ajout d'une hauteur définie

--- components/layout/global/Footer.js
+++ components/layout/global/Footer.js
@@ -8,7 +8,7 @@ const Footer = () => {
   const actualYear = new Date().getFullYear();
 
   return (
-    <Layout.Footer style={{ textAlign: 'center' }}>
+    <Layout.Footer style={{ textAlign: 'center', height: 70 }}>
       {/* eslint-disable-next-line */}
       Biblidev 2019 - {actualYear}
     </Layout.Footer>
